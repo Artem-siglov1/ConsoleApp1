9ad137937e2b6ca86437dac2b2c56fb35dcc8bc7
Artem-siglov1 <artemsiglov@gmail.com>
2023-01-24 22:05:14 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,477 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string myLog, myPas, log, pas;
+            //myLog = "Artem_Siglov1";
+            //myPas = "Qwertyuiop*0";
+            //Console.Write("Введите логин:");
+            //log = Convert.ToString(Console.ReadLine());
+            //Console.Write("Введите пароль:");
+            //pas = Convert.ToString(Console.ReadLine());
+            //bool c = log.Length > 6 && pas.Length > 6;
+
+            //if (c == true && myLog == log && myPas == pas)
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Green;
+            //    Console.WriteLine("Доступ разрешен");
+            //}
+            //else if (c == true && myLog == log && myPas != pas)
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Red;
+            //    Console.WriteLine("Неверный пароль");
+            //}
+            //else if (c == true && myLog != log && myPas == pas)
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Red;
+            //    Console.WriteLine("Неверный логин");
+            //}
+            //else if (c == true && myLog != log && myPas != pas)
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Red;
+            //    Console.WriteLine("Неверный логин и пароль");
+            //}
+            //else
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Red;
+            //    Console.WriteLine("Логин и пароль должны быть длинее 6 символов");
+            //}
+            //Console.ReadLine();
+
+            //  double a, b, c;
+            //Задание 1
+            //Console.Write("Введите число:");
+            //a = Convert.ToDouble(Console.ReadLine());
+            //if (3%a==0)
+            //{
+            // Console.WriteLine("Ваше число является делителем 3");
+            //}
+            //else
+            //{
+            // Console.WriteLine("Ваше число не является делителем 3");
+            //}
+
+            //задание 2
+            //Console.Write("Введите число:");
+            //a = Convert.ToDouble(Console.ReadLine());
+            //if (a % 4 == 0)
+            //{
+            // Console.WriteLine("Ваше число является кратным 4");
+            //}
+            //else
+            //{
+            // Console.WriteLine("Ваше число не является кратным 4");
+            //}
+
+            //задание 3
+            //Console.Write("Введите 1 число:");
+            //a = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Введите 2 число:");
+            //b = Convert.ToDouble(Console.ReadLine());
+            //if (b % a == 0)
+            //{
+            // Console.WriteLine("Ваше число 1 является делителем 2 числа");
+            //}
+            //else
+            //{
+            // Console.WriteLine("Ваше число 1 не является делителем 2 числа");
+            //}
+
+
+            //Задание 4
+            //Console.Write("Введите число:");
+            //a = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Введите число:");
+            //b = Convert.ToDouble(Console.ReadLine());
+            //c = a % b;
+            //Console.WriteLine(c);
+
+
+            //Задание 5 
+            //Console.Write("Введите число:");
+            //a = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Введите число:");
+            //b = Convert.ToDouble(Console.ReadLine());
+            //if (a < b)
+            //{
+            //    double f = a;
+            //    a = a + b;
+            //    Console.WriteLine("a= "+a);
+            //    b = f * b;
+            //    Console.WriteLine("b= " + b);
+            //}
+            //else
+            //{
+            //    double f = a;
+            //    a = a * b;
+            //    Console.WriteLine("a= " + a);
+            //    b = f + b;
+            //    Console.WriteLine("b= " + b);
+            //}
+            //Задание 6
+            //Console.Write("Введите число:");
+            //a = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Введите число:");
+            //b = Convert.ToDouble(Console.ReadLine());
+            //if (a < b)
+            //{
+            //    double f = a;
+            //    a = (a + b)/2;
+            //    Console.WriteLine("a= " + a);
+            //    b = (f * b)*2;
+            //    Console.WriteLine("b= " + b);
+            //}
+            //else
+            //{
+            //    double f = a;
+            //    a = (a * b)*2;
+            //    Console.WriteLine("a= " + a);
+            //    b = (f + b)/2;
+            //    Console.WriteLine("b= " + b);
+            //}
+            // Console.ReadKey();
+            //double x =Convert.ToDouble(Console.ReadLine());
+            //if (x >= 0)
+            //{
+            //   x= Math.Sqrt(Math.Pow(x, 2) - 1);
+            //    Console.WriteLine(x);
+            //}
+            //else
+            //{
+            //   x=Math.Pow(x, 2) + 25;
+            //    Console.WriteLine(x);
+            //}
+
+            //   double x =Convert.ToDouble(Console.ReadLine());
+            //if (x >= 0)
+            //{
+            //   x=Math.Sqrt(x) + 2 * Math.Sin(x);
+            //    Console.WriteLine(x);
+            //}
+            //else
+            //{
+            //    x=6 - Math.Pow(x, 2);
+            //    Console.WriteLine(x);
+            //}
+
+            //    if (x >= 0)
+            //{
+            //   x=Math.Sqrt(x) + 2 * Math.Sin(x);
+            //    Console.WriteLine(x);
+            //}
+            //if (x < 0)
+            //{
+            //    x=6 - Math.Pow(x, 2);
+            //    Console.WriteLine(x);
+            //}
+
+
+
+
+
+            //for(int g=0;g<=17;g++)
+            //{
+            //    Console.Write("*");
+            //}
+            //int a = 0;
+
+            //while (a <= 17)
+            //{
+            //    Console.Write("*");
+            //    a++;
+            //}
+            //int a = 0;
+
+            //do
+            //{
+            //    Console.Write("*");
+            //    a++;
+            //}
+            //while (a <= 17);
+
+
+
+
+            //for(int g=0;g<=24;g++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+            //int a = 0;
+
+            //while (a <= 24)
+            //{
+            //    Console.WriteLine("*");
+            //    a++;
+            //}
+            //int a = 0;
+
+            //do
+            //{
+            //    Console.WriteLine("*");
+            //    a++;
+            //}
+            //while (a <= 24);
+
+
+
+
+
+
+
+
+            //for (int a = 0; a <= 3; a++)
+            //   {
+            //   Console.WriteLine("");
+            //   }
+            //for (int g = 0; g <= 19; g++)
+            //{
+            //    Console.Write("*");
+            //}
+
+            //int a = 0;
+            //int g=0;
+
+            //while (g <=3)
+            //{
+            //    Console.WriteLine("");
+            //    g++;
+            //}
+            //while (a <= 19)
+            //{
+            //    Console.Write("*");
+            //    a++;
+            //}
+
+            //int a = 0;
+            //int g=0;
+
+            //do
+            //{
+            //    Console.WriteLine("");
+            //    a++;
+            //}
+            //while (a <= 3);
+            //do
+            //{
+            //    Console.Write("*");
+            //    a++;
+            //}
+            //while (a <= 19);
+
+
+
+
+
+
+            //for (int a = 0; a <= 4; a++)
+            //   {
+            //   Console.Write("");
+            //   }
+            //for (int g = 0; g <= 17; g++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            //int a = 0;
+            //int g=0;
+
+            //while (g <=4)
+            //{
+            //    Console.Write("");
+            //    g++;
+            //}
+            //while (a <= 17)
+            //{
+            //    Console.WriteLine("*");
+            //    a++;
+            //}
+
+            //int a = 0;
+            //int g = 0;
+
+            //do
+            //{
+            //    Console.Write("");
+            //    a++;
+            //}
+            //while (a <= 4);
+            //do
+            //{
+            //    Console.WriteLine("*");
+            //    a++;
+            //}
+            //while (a <= 17);
+
+
+
+            //for (int a = 0; a <= 4; a++)
+            //{
+            //    for (int g = 0; g <= 14; g++)
+            //    {
+            //        Console.Write("*");
+
+            //    }
+            //    Console.WriteLine("");
+            //}
+
+
+
+            //for (int a = 0; a <= 3; a++)
+            //{
+            //    for (int g = 0; g <= 6; g++)
+            //    {
+            //        Console.Write("*");
+
+            //    }
+            //    Console.WriteLine("");
+            //    Console.WriteLine("");
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Задание1
+            //for (int a = 0; a < 10; a++)
+            //{
+            // Console.Write("#");
+            //}
+            //Console.WriteLine("");
+            //for (int a = 0; a < 12; a++)
+            //{
+            // Console.WriteLine("#");
+            //}
+
+
+            //Задание2
+            //for (int a = 0; a < 10; a++)
+            //{
+            //    Console.Write("#");
+            //}
+            //Console.WriteLine("");
+            //for (int a = 0; a < 12; a++)
+            //{
+
+            //    for (int g = 0; g < 9; g++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    Console.WriteLine("#");
+            //}
+
+
+
+            //Задание3
+            //for(int a=0;a<14;a++)
+            //{
+            // Console.WriteLine("#");
+            //}
+            //for(int a=0;a<12;a++)
+            //{
+            // Console.Write("#");
+            //}
+
+
+            //Задание4
+            //for (int a = 0; a < 12; a++)
+            //{
+
+            // for (int g = 0; g < 9; g++)
+            // {
+            // Console.Write(" ");
+            // }
+            // Console.WriteLine("#");
+            //}
+            // for(int a=0;a<10;a++)
+            //{
+            // Console.Write("#");
+            //}
+
+
+
+            //Задание5
+            //for (int a = 0; a < 10; a++)
+            //{
+            //    Console.Write("#");
+            //}
+            //Console.WriteLine();
+            //for (int a = 0; a < 10; a++)
+            //{
+            //    Console.Write("#");
+            //    for(int d=0;d<=7;d++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    Console.WriteLine("#");
+            //}
+            //for (int a = 0; a < 10; a++)
+            //{
+            //    Console.Write("#");
+            //}
+
+
+
+
+            //Задание 6
+            //for (int i = 4; i >= 0; i--)
+            //{
+            //    for (int j = 0; j <= i; j++)
+            //        Console.Write(j + " ");
+            //    Console.WriteLine();
+            //}
+
+            //Задание 7
+
+
+            //for (int d = 0; d < 4; d++)
+            //{
+            //    for (int a = 0; a < 5; a++)
+            //    {
+            //        Console.Write("2" + " ");
+            //    }
+            //    Console.WriteLine("");
+            //}
+            Console.ReadKey();
+        }   
+        
+    }
+}
